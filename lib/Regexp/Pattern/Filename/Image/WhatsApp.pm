9c51e37dcc84a8e56a117a559fc104c457f2ae49
perlancar (on pc-home) <perlancar@gmail.com>
2022-05-08 18:04:22 +0700

    Initial commit

---
+++ lib/Regexp/Pattern/Filename/Image/WhatsApp.pm
@@ -0,0 +1,37 @@
+package Regexp::Pattern::Filename::Image::WhatsApp;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+#use utf8;
+
+use Filename::Image ();
+
+our %RE;
+
+$RE{filename_image_whatsapp} = {
+    summary => 'Image filename saved by WhatsApp',
+    pat => qr/\AIMG-[0-9]{8}-WA[0-9]{4,}\.(?:JPG|jpg)\z/,
+    examples => [
+        {str=>'foo.jpg', matches=>0, summary=>'No pattern'},
+        {str=>'IMG-20210922-WA0001.jpg', matches=>1},
+        {str=>'IMG-20210922-WA0001.txt', matches=>0, summary=>'Wrong extension'},
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+
+=head1 prepend:SEE ALSO
+
+L<Regexp::Pattern::Filename::Video::WhatsApp>
+
+L<Regexp::Pattern::Filename::Media::WhatsApp>
